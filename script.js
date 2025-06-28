2ef174d66a5397878d5b5acf37a443c5ffe858d5
Fabio Zelak <shimask@gmail.com>
2025-06-28 20:09:44 -0300

    Criar script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Scroll suave ao clicar no menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    const target = document.querySelector(this.getAttribute('href'))
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' })
+    }
+  })
+})
